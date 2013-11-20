f55d18b02d273aeaa1bbfb4bd80619c3e73043b1
Lukas Härtel <lukashaertel@googlemail.com>
2013-11-20 17:01:29 +0100

    Implementation des WFJ-Streamers

---
+++ MobileSensingCollector/src/mobsens/collector/consumers/WFJStreamingConsumer.java
@@ -0,0 +1,30 @@
+package mobsens.collector.consumers;
+
+import java.io.PrintStream;
+
+import mobsens.collector.wfj.WFJ;
+import android.content.ContextWrapper;
+
+public class WFJStreamingConsumer extends PrintStreamingConsumer<WFJ>
+{
+	private PrintStream printStream;
+
+	public WFJStreamingConsumer(ContextWrapper contextWrapper)
+	{
+		super(contextWrapper);
+		
+		printStream = null;
+	}
+
+	@Override
+	protected void redirectPrint(PrintStream printStream)
+	{
+		this.printStream = printStream;
+	}
+
+	@Override
+	protected void write(WFJ item)
+	{
+		printStream.println(item.generate());
+	}
+}
