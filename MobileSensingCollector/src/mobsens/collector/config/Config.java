2e8ee615a878cbe6ed92d4ade4c29fe21749f2e9
Lukas Härtel <lukashaertel@googlemail.com>
2013-12-01 16:53:42 +0100

    Config eingeführt

---
+++ MobileSensingCollector/src/mobsens/collector/config/Config.java
@@ -0,0 +1,18 @@
+package mobsens.collector.config;
+
+public interface Config
+{
+	public static final double FREQUENCY_ACCELEROMETER = 50.0;
+
+	public static final double FREQUENCY_GYROSCOPE = 50.0;
+
+	public static final double FREQUENCY_LINEAR_ACCELEROMETER = 50.0;
+
+	public static final double FREQUENCY_GRAVITY = 50.0;
+
+	public static final double FREQUENCY_MAGNETIC_FIELD = 50.0;
+	
+	public static final double FREQUENCY_LOCATION = 2.0; 
+	
+	public static final boolean CONFIG_GPS_ONLY = true;
+}
