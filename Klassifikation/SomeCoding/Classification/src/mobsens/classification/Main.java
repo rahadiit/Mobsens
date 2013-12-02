26034358e53c9dd1825f581a5133cbd22b4b1cdd
hennyb <henny@uni-koblenz.de>
2013-12-03 00:29:50 +0100

    kleine Aenderungen, Bugbeseitigung

--- Klassifikation/SomeCoding/Classification/src/mobsens/classification/Main.java
+++ Klassifikation/SomeCoding/Classification/src/mobsens/classification/Main.java
@@ -1,6 +1,5 @@
 package mobsens.classification;
 
-import java.io.File;
 import java.util.ArrayList;
 
 import com.sun.jersey.api.client.Client;
@@ -10,28 +9,29 @@ import mobsens.classification.data.Recording;
 import mobsens.classification.data.Sensor;
 import mobsens.classification.data.URLS;
 import mobsens.classification.input.CSV;
-import mobsens.classification.input.RESTFul;
+import mobsens.classification.input.RESTful;
 import mobsens.classification.output.PrettyPrint;
 
 public class Main {
 
 	private static final String username = "mlukas@gmx.net";
 	private static final String password = "12345678";
-	
+
 	public static void main(String[] args) {
-
-		ArrayList<Location> locations = CSV.csvToLocation(new File("locSpazieren.csv"));
-		PrettyPrint.print(locations);
-
-		Client client = RESTFul.login(URLS.LOGIN.getURL(), username, password);
-
-		ArrayList<Recording> recordings = RESTFul.recordingOutput(client, URLS.LIST_RECORDINGS.getURL());
 		
-		for(Recording rec: recordings){
-			rec.prettyPrint();
+		Client client = RESTful.login(URLS.LOGIN.getURL(), username, password);
+		ArrayList<Recording> recordings = RESTful.recordingOutput(client,URLS.LIST_RECORDINGS.getURL());
+
+		for (Recording rec : recordings) {
+			client = RESTful.login(URLS.LOGIN.getURL(), username, password);
+			String csv=RESTful.getCSV(client, rec.getId(), URLS.CSV.getURL(),Sensor.LOCATIONS);
+			
+			if(csv!=null){
+				ArrayList<Location> csvTest = CSV.csvToLocation(csv);
+				PrettyPrint.print(csvTest);
+			}
 		}
 		
-		System.out.println(RESTFul.getCSV(client, 7,URLS.CSV.getURL(),Sensor.ACCELEROMETERS ));
-		
 	}
+	
 }
