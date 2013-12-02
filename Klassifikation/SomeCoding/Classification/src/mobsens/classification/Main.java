0d3449aab9b85a5ca429c32843a8e577df4501d4
hennyb <henny@uni-koblenz.de>
2013-12-02 18:23:46 +0100

    all append
    Signed-off-by: hennyb <henny@uni-koblenz.de>

--- Klassifikation/SomeCoding/Classification/src/mobsens/classification/Main.java
+++ Klassifikation/SomeCoding/Classification/src/mobsens/classification/Main.java
@@ -4,48 +4,34 @@ import java.io.File;
 import java.util.ArrayList;
 
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 import mobsens.classification.data.Location;
 import mobsens.classification.data.Recording;
+import mobsens.classification.data.Sensor;
+import mobsens.classification.data.URLS;
 import mobsens.classification.input.CSV;
 import mobsens.classification.input.RESTFul;
 import mobsens.classification.output.PrettyPrint;
 
 public class Main {
 
+	private static final String username = "mlukas@gmx.net";
+	private static final String password = "12345678";
+	
 	public static void main(String[] args) {
 
-		ArrayList<Location> locations = CSV.csvToLocation(new File(
-				"locSpazieren.csv"));
+		ArrayList<Location> locations = CSV.csvToLocation(new File("locSpazieren.csv"));
 		PrettyPrint.print(locations);
 
-		String URL_LOGIN = "http://mobilesensing.west.uni-koblenz.de/users/sign_in.json";
-		String URL_DATA = "http://mobilesensing.west.uni-koblenz.de/recordings.json";
-		
-		String username = "mlukas@gmx.net";
-		String password = "12345678";
+		Client client = RESTFul.login(URLS.LOGIN.getURL(), username, password);
 
-		Client client = RESTFul.login(URL_LOGIN, username, password);
-
-		ArrayList<Recording> recordings = RESTFul.recordingOutput(client, URL_DATA);
+		ArrayList<Recording> recordings = RESTFul.recordingOutput(client, URLS.LIST_RECORDINGS.getURL());
 		
 		for(Recording rec: recordings){
 			rec.prettyPrint();
 		}
 		
+		System.out.println(RESTFul.getCSV(client, 7,URLS.CSV.getURL(),Sensor.ACCELEROMETERS ));
+		
 	}
 }
-
-// $ curl -v -H "Accept: application/json" -H "Content-type: application/json"
-// -c cookies.txt -X POST --data
-// '{"user":{"email":"your@ema.il","password":"yourpassword"}}'
-// 'http://localhost:3000/users/sign_in.json'
-//
-// IntentUpload.startService(Controller.this, file.getName(),
-// "http://mobilesensing.west.uni-koblenz.de/users/sign_in.json",
-// "mlukas@gmx.net", "12345678",
-// "http://mobilesensing.west.uni-koblenz.de/recordings/upload", file,
-// "application/text", "*/*", true);
-
