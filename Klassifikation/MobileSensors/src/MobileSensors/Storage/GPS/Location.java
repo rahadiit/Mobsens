498bccfa84b90c5f667adb2f14e2d6205ab56603
lukas <mlukas@gmx.net>
2013-12-06 01:46:36 +0100

    ruby java bridge interface

---
+++ Klassifikation/MobileSensors/src/MobileSensors/Storage/GPS/Location.java
@@ -0,0 +1,80 @@
+package MobileSensors.Storage.GPS;
+
+public class Location {
+
+	private long time;
+	private double latitude;
+	private double longitude;
+	private double altitude;
+	private double speed;
+	private double bearing;
+	private double accuracy;
+
+	public Location(long time, double latitude, double longitude,
+			double altitude, double speed, double bearing, double accuracy) {
+		this.time = time;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.altitude = altitude;
+		this.speed = speed;
+		this.bearing = bearing;
+		this.accuracy = accuracy;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getAltitude() {
+		return altitude;
+	}
+
+	public void setAltitude(double altitude) {
+		this.altitude = altitude;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public double getBearing() {
+		return bearing;
+	}
+
+	public void setBearing(double bearing) {
+		this.bearing = bearing;
+	}
+
+	public double getAccuracy() {
+		return accuracy;
+	}
+
+	public void setAccuracy(double accuracy) {
+		this.accuracy = accuracy;
+	}
+
+}
---
+++ Klassifikation/MobileSensors/src/MobileSensors/Storage/Sensors/Accelerometer.java
@@ -0,0 +1,7 @@
+package MobileSensors.Storage.Sensors;
+
+import MobileSensors.Storage.Sensors.Sensor.Sensor;
+
+public class Accelerometer extends Sensor {
+
+}
