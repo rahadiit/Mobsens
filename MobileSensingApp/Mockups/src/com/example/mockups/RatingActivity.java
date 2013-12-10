4e089db2e754acf9ae150f6b3af3572ccebea586
Lukas Härtel <lukashaertel@googlemail.com>
2013-12-10 11:51:41 +0100

    Layout mit Radio-Group

--- MobileSensingApp/Mockups/src/com/example/mockups/RatingActivity.java
+++ MobileSensingApp/Mockups/src/com/example/mockups/RatingActivity.java
@@ -2,47 +2,58 @@ package com.example.mockups;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 
-public class RatingActivity extends Activity{
-	
-	
+public class RatingActivity extends Activity
+{
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_rating);
 	}
-	
-	public void radioButton(View v){
-		
-		RadioButton b = (RadioButton) findViewById(R.id.radio1);
-		RadioButton c = (RadioButton) findViewById(R.id.radio0);
-		
-		if (v.getId() == R.id.radio0){
-			if(c.isChecked() == true){
-				c.setChecked(false);
-				b.setChecked(true);
-			}else {
-				c.setChecked(true);
-				b.setChecked(false);
-			}
-//			if((c.isChecked() != true)){
-//				c.setChecked(true);
-//				b.setChecked(false);
+//
+//	public void radioButton(View v)
+//	{
+//
+//		RadioButton radio0 = (RadioButton) findViewById(R.id.radio0);
+//		RadioButton radio1 = (RadioButton) findViewById(R.id.radio1);
+//
+//		if (v.getId() == R.id.radio0)
+//		{
+//			if (radio0.isChecked() == true)
+//			{
+//				radio0.setChecked(false);
+//				radio1.setChecked(true);
 //			}
-		}
-		
-		if(v.getId() == R.id.radio1){
-			if(b.isChecked() == true){
-				b.setChecked(false);
-				c.setChecked(true);
-			}else {
-//			if((b.isChecked() != true)){
-				b.setChecked(true);
-				c.setChecked(false);
-			}
-		}
-		
-	}
+//			else
+//			{
+//				radio0.setChecked(true);
+//				radio1.setChecked(false);
+//			}
+//			// if((c.isChecked() != true)){
+//			// c.setChecked(true);
+//			// b.setChecked(false);
+//			// }
+//		}
+//
+//		if (v.getId() == R.id.radio1)
+//		{
+//			if (radio1.isChecked() == true)
+//			{
+//				radio1.setChecked(false);
+//				radio0.setChecked(true);
+//			}
+//			else
+//			{
+//				// if((b.isChecked() != true)){
+//				radio1.setChecked(true);
+//				radio0.setChecked(false);
+//			}
+//		}
+//
+//	}
 }
