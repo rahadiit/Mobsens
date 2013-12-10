b4e74a6d29648450b88a26988c8279db3bc79093
Lukas Härtel <lukashaertel@googlemail.com>
2013-12-10 11:21:17 +0100

    Import Mockups

---
+++ MobileSensingApp/Mockups/src/com/example/mockups/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.example.mockups;
+
+import android.app.*;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity {
+int state = 0;	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+	
+	public void intention(View v) {
+		
+		
+		Button b = (Button) findViewById(R.id.intention);
+		if (state == 0) {
+			b.setText("Sport");
+			state++;
+		} else {
+			if (state == 1) {
+				b.setText("Alltag");
+				state++;
+			} else {
+				if (state == 2) {
+					b.setText("Freizeit");
+					state = 3;
+
+				} else {
+					if (state == 3) {
+						b.setText("None");
+						state = 0;
+					}
+				}
+			}
+		}
+
+	}
+}
---
+++ MobileSensingApp/Mockups/src/com/example/mockups/RatingActivity.java
@@ -0,0 +1,48 @@
+package com.example.mockups;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+public class RatingActivity extends Activity{
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rating);
+	}
+	
+	public void radioButton(View v){
+		
+		RadioButton b = (RadioButton) findViewById(R.id.radio1);
+		RadioButton c = (RadioButton) findViewById(R.id.radio0);
+		
+		if (v.getId() == R.id.radio0){
+			if(c.isChecked() == true){
+				c.setChecked(false);
+				b.setChecked(true);
+			}else {
+				c.setChecked(true);
+				b.setChecked(false);
+			}
+//			if((c.isChecked() != true)){
+//				c.setChecked(true);
+//				b.setChecked(false);
+//			}
+		}
+		
+		if(v.getId() == R.id.radio1){
+			if(b.isChecked() == true){
+				b.setChecked(false);
+				c.setChecked(true);
+			}else {
+//			if((b.isChecked() != true)){
+				b.setChecked(true);
+				c.setChecked(false);
+			}
+		}
+		
+	}
+}
