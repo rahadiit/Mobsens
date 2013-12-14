fa8b8fb8bd62323c8640c43db7d3a9273d05664d
hennyb <henny@uni-koblenz.de>
2013-12-14 16:05:13 +0100

    Errechnung von Location-Attributen

--- Klassifikation/MobileSensors/src/MobileSensors/Storage/Sensors/Location.java
+++ Klassifikation/MobileSensors/src/MobileSensors/Storage/Sensors/Location.java
@@ -2,7 +2,7 @@ package MobileSensors.Storage.Sensors;
 
 import MobileSensors.Storage.Sensors.Sensor.Sensor;
 
-public class Location extends Sensor{
+public class Location extends Sensor {
 
 	private double latitude;
 	private double longitude;
@@ -10,6 +10,17 @@ public class Location extends Sensor{
 	private double speed;
 	private double bearing;
 	private double accuracy;
+	
+	private double distanceCalc;
+	private double distanceSumCalc;
+	
+	private double distanceCalc2;
+	private double distanceSumCalc2;
+	
+	private double speedCalc;
+	private double accelerationCalc;
+	private double jerkCalc;
+	private long timeCalc;
 
 	public Location(long time, double latitude, double longitude,
 			double altitude, double speed, double bearing, double accuracy) {
@@ -70,8 +81,75 @@ public class Location extends Sensor{
 		this.accuracy = accuracy;
 	}
 
-	public double[] getCoordinates(){
-		double[] result = {latitude,longitude};
+	/*
+	 * @return latitute, longitude
+	 */
+	public double[] getCoordinates() {
+		double[] result = { latitude, longitude };
 		return result;
+	}
+
+	public double getSpeedCalc() {
+		return speedCalc;
+	}
+
+	public void setSpeedCalc(double speedCalc) {
+		this.speedCalc = speedCalc;
+	}
+
+	public double getAccelerationCalc() {
+		return accelerationCalc;
+	}
+
+	public void setAccelerationCalc(double accelerationCalc) {
+		this.accelerationCalc = accelerationCalc;
+	}
+
+	public long getTimeCalc() {
+		return timeCalc;
+	}
+
+	public void setTimeCalc(long timeCalc) {
+		this.timeCalc = timeCalc;
+	}
+
+	public double getDistanceCalc() {
+		return distanceCalc;
+	}
+
+	public void setDistanceCalc(double distanceCalc) {
+		this.distanceCalc = distanceCalc;
+	}
+
+	public double getJerkCalc() {
+		return jerkCalc;
+	}
+
+	public void setJerkCalc(double jerkCalc) {
+		this.jerkCalc = jerkCalc;
+	}
+
+	public double getDistanceSumCalc() {
+		return distanceSumCalc;
+	}
+
+	public void setDistanceSumCalc(double distanceSumCalc) {
+		this.distanceSumCalc = distanceSumCalc;
+	}
+
+	public double getDistanceCalc2() {
+		return distanceCalc2;
+	}
+
+	public void setDistanceCalc2(double distanceCalc2) {
+		this.distanceCalc2 = distanceCalc2;
+	}
+
+	public double getDistanceSumCalc2() {
+		return distanceSumCalc2;
+	}
+
+	public void setDistanceSumCalc2(double distanceSumCalc2) {
+		this.distanceSumCalc2 = distanceSumCalc2;
 	}
 }
