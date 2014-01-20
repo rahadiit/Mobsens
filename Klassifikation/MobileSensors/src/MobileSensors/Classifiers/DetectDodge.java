ede62b07d105cf716555866c8784857963f77411
hennyb <henny@uni-koblenz.de>
2014-01-20 17:19:57 +0100

    dodge optimiert. Refactoring ist ein MUSS

--- Klassifikation/MobileSensors/src/MobileSensors/Classifiers/DetectDodge.java
+++ Klassifikation/MobileSensors/src/MobileSensors/Classifiers/DetectDodge.java
@@ -2,7 +2,9 @@ package MobileSensors.Classifiers;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
+import MobileSensors.Enums.Axis;
 import MobileSensors.Storage.Event.Event;
 import MobileSensors.Storage.Sensors.Accelerometer;
 
@@ -26,98 +28,174 @@ public class DetectDodge implements EventClassifier {
 			accel = (ArrayList<Accelerometer>) Accelerometer.window(data, time,
 					time + span);
 
-			if (accel.size() > 2) {
-				double value = accel.get(0).getX();
-				// initial boolean values <=,prevent undefined behavior
-				boolean allLower = accel.get(1).getX() <= value ? true : false;
-				boolean allHigher = !allLower;
+			if (accel.size() <= 25)
+				continue;
 
-				// kontrolliert, ob alle Werte in diesem Zeitfenster kleiner
-				// sind
-				for (int j = 2; j < accel.size(); j++) {
-					double compareV = accel.get(j).getX();
+			double value = accel.get(0).getX();
+			// initial boolean values <=,prevent undefined behavior
+			boolean allLower = accel.get(1).getX() <= value ? true : false;
+			boolean allHigher = !allLower;
 
-					if (compareV < value) {
-						if (allHigher) {
-							allHigher = false;
-							break;
-						}
+			// kontrolliert, ob alle Werte in diesem Zeitfenster kleiner
+			// sind
+			for (int j = 2; j < accel.size(); j++) {
+				double compareV = accel.get(j).getX();
 
-					} else if (compareV > value) {
-						if (allLower) {
-							allLower = false;
-							break;
-						}
+				if (compareV < value) {
+					if (allHigher) {
+						allHigher = false;
+						break;
 					}
-				}
 
-				// falls alle werte kleiner oder groesser sind, als erster wert
-				// finde maximale differenz
-
-				if (allLower || allHigher) {
-					double maximalDiff = 0;
-
-					for (int j = 1; j < accel.size(); j++) {
-
-						double diff = value - accel.get(j).getX();
-
-						if (allLower) {
-							if (diff > maximalDiff) {
-								maximalDiff = diff;
-								//System.out.println("v "+value+ " - ac"+accel.get(j).getX()+ "mD "+maximalDiff);
-							}
-
-						} else if (allHigher) {
-							if (diff < maximalDiff) {
-								maximalDiff = diff;
-								//System.out.println("v "+value+ " - ac"+accel.get(j).getX()+ "mD "+maximalDiff);
-							}
-						}
-					}
-					// wenn alle werte kleiner||groesser sind && die differenz
-					// zum anfangs-punkt sehr hoch ist
-
-					if (Math.abs(maximalDiff) >= 7.5) {
-						
-						// neues Intervall erstellen welches nach den getesten
-						// werten anfaeng und auch eine halbe sekunde dauert
-						// in diesem Intervall muessen Messwerte existieren,
-						// welche Nahe dem des Anfangs-Punktes sind
-
-						// Intervall nach dem letzten Messwert des vorherigen
-						// Intervalls
-						long span2 = 400;
-						long time2 = accel.get(accel.size() - 1).getTime();
-						ArrayList<Accelerometer> accel2 = new ArrayList<>();
-						accel2 = (ArrayList<Accelerometer>) Accelerometer
-								.window(data, time2 + 1, time2 + span2);
-
-						boolean foundValue = false;
-						// laufe von hinten. erhofft schnellere berechnung
-						for (int j = accel2.size() - 1; j > 0; j--) {
-
-							double compValue = accel2.get(j).getX();
-
-							if ((compValue > value - 0.8 && compValue < value + 0.8)
-									|| (allLower && compValue > value)
-									|| (allHigher && compValue < value)) {
-								foundValue = true;
-								break;
-							}
-						}
-
-						if (foundValue) {
-							// i kann auf letzten Punkt des Intervalls gesetzt
-							// werden
-							i = data.indexOf(accel.get(accel.size() - 1)) + 1;
-							events.add(new Event(data.get(i).getTime(),
-									MobileSensors.Storage.Event.EventType.DODGE));
-						}
+				} else if (compareV > value) {
+					if (allLower) {
+						allLower = false;
+						break;
 					}
 				}
 			}
+
+			// letzter Wert muss kleiner bzw groesser sein als mittlerer
+			// Wert
+
+			if (allLower) {
+				if (accel.get(accel.size() - 1).getX() > accel.get(
+						(accel.size() - 1) / 2).getX())
+					allLower = false;
+			}
+			if (allHigher) {
+				if (accel.get(accel.size() - 1).getX() < accel.get(
+						(accel.size() - 1) / 2).getX())
+					allHigher = false;
+			}
+
+			// falls alle werte kleiner oder groesser sind, als erster wert
+			// finde maximale differenz
+
+			if (allLower || allHigher) {
+				// System.out.println(data.get(i).getTime() + " " +
+				// allLower);
+				double maximalDiff = 0;
+
+				for (int j = 1; j < accel.size(); j++) {
+
+					double diff = value - accel.get(j).getX();
+
+					if (allLower) {
+						if (diff > maximalDiff) {
+							maximalDiff = diff;
+							// System.out.println("v "+value+
+							// " - ac"+accel.get(j).getX()+
+							// "mD "+maximalDiff);
+						}
+
+					} else if (allHigher) {
+						if (diff < maximalDiff) {
+							maximalDiff = diff;
+							// System.out.println("v "+value+
+							// " - ac"+accel.get(j).getX()+
+							// "mD "+maximalDiff);
+						}
+					}
+				}
+				// wenn alle werte kleiner||groesser sind && die differenz
+				// zum anfangs-punkt sehr hoch ist
+
+				if (Math.abs(maximalDiff) >= 7) {
+//					System.out.println("");
+//					System.out.println("max diff hat gepasst" +data.get(i).getTime());
+
+					// neues Intervall erstellen welches nach den getesten
+					// werten anfaeng und auch eine halbe sekunde dauert
+					// in diesem Intervall muessen Messwerte existieren,
+					// welche Nahe dem des Anfangs-Punktes sind
+
+					// Intervall nach dem letzten Messwert des vorherigen
+					// Intervalls
+					long span2 = 400;
+					long time2 = accel.get(accel.size() - 1).getTime();
+					ArrayList<Accelerometer> accel2 = new ArrayList<>();
+					accel2 = (ArrayList<Accelerometer>) Accelerometer.window(
+							data, time2 + 1, time2 + span2);
+
+					boolean foundValue = false;
+					// laufe von hinten. erhofft schnellere berechnung
+					for (int j = accel2.size() - 1; j > 0; j--) {
+
+						double compValue = accel2.get(j).getX();
+
+						if ((compValue > value - 2 && compValue < value + 2)
+								|| (allLower && compValue > value)
+								|| (allHigher && compValue < value)) {
+							foundValue = true;
+							break;
+						}
+					}
+
+//					if (foundValue) {
+//						
+//						System.out.println("End-wert wurde auch gefunden");
+//						
+//						ArrayList<Accelerometer> complete = new ArrayList<>();
+//						complete.addAll(accel);
+//						complete.addAll(accel2);
+//						
+//						System.out.println("complete" +complete.size());
+//
+//						for (Accelerometer a : complete) {
+//							a.setInterestedAxis(Axis.X);
+//						}
+//
+//						try {
+//							@SuppressWarnings("unchecked")
+//							double highestValue = Collections.max(complete)
+//									.getX();
+//							@SuppressWarnings("unchecked")
+//							double lowestValue = Collections.min(complete)
+//									.getX();
+//
+//							foundValue = Math.abs(highestValue - lowestValue) > 7;
+//
+//							if(!foundValue)
+//								System.out.println("abstand nicht gross genug");
+//							
+//							if (foundValue) {
+//								System.out.println("abschnitt1");
+//								for (Accelerometer a : accel) {
+//									System.out.println(a.getTime() + " :: "
+//											+ a.getX());
+//								}
+//								System.out.println("abschnitt2");
+//								for (Accelerometer a : accel2) {
+//									System.out.println(a.getTime() + " :: "
+//											+ a.getX());
+//								}
+//								System.out.println("");
+//							}
+//
+//						} catch (Exception e) {
+//							e.printStackTrace();
+//						}
+//					}else{
+//						System.out.println("endwert wurde nicht gefunden");
+//					}
+
+					if (foundValue) {
+						
+						
+						// i kann auf letzten Punkt des Intervalls
+						// gesetzt
+						// werden
+						events.add(new Event(data.get(i).getTime(),
+								MobileSensors.Storage.Event.EventType.DODGE));
+						i = data.indexOf(accel.get(accel.size() - 1)) + 1;
+
+					}
+				}
+			}
+
 		}
-		System.out.println(events.size() + " dodges found");
+		//System.out.println(events.size() + " dodges found");
 		return events;
 	}
 }
