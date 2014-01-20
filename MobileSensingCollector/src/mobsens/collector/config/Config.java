f651d9eb5c01a3d92d3ae4f216d48d4aef7846ce
Lukas Härtel <lukashaertel@googlemail.com>
2014-01-20 23:51:32 +0100

    Mehr Adjustierungen

--- MobileSensingCollector/src/mobsens/collector/config/Config.java
+++ MobileSensingCollector/src/mobsens/collector/config/Config.java
@@ -2,22 +2,24 @@ package mobsens.collector.config;
 
 public interface Config
 {
-	public static final double FREQUENCY_ACCELEROMETER = 50.0;
+	public static final double OFF = -1.0;
 
-	public static final double FREQUENCY_GYROSCOPE = 50.0;
+	public static final double FREQUENCY_ACCELEROMETER = 100.0;
 
-	public static final double FREQUENCY_LINEAR_ACCELEROMETER = 50.0;
+	public static final double FREQUENCY_GYROSCOPE = 100.0;
 
-	public static final double FREQUENCY_GRAVITY = 50.0;
+	public static final double FREQUENCY_LINEAR_ACCELEROMETER = OFF;
 
-	public static final double FREQUENCY_MAGNETIC_FIELD = 50.0;
+	public static final double FREQUENCY_GRAVITY = OFF;
 
-	public static final double FREQUENCY_LOCATION = 5.0;
+	public static final double FREQUENCY_MAGNETIC_FIELD = 100.0;
+
+	public static final double FREQUENCY_LOCATION = 4.0;
 
 	/**
 	 * Auf Erdbeschleunigung innerhalb einer Millisekunde
 	 */
-	public static final double ACCELEROMETER_MAX_DDT = 10.0f / 0.001;
+	public static final double ACCELEROMETER_MAX_DDT = 10.0f / 0.01;
 
 	/**
 	 * Ungefähr 250km/h
