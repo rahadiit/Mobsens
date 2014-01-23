2d99e46aea7998167cadf0a95f54ebedc57a7f5b
hennyb <henny@uni-koblenz.de>
2014-01-23 14:56:10 +0100

    main

--- Klassifikation/MobileSensorsTest/src/MobileSensors/Test/Main.java
+++ Klassifikation/MobileSensorsTest/src/MobileSensors/Test/Main.java
@@ -24,27 +24,27 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		for (int j = 1; j < 10; j++) {
+		for(int j=1;j<10;j++){
+			
+		
+		
+		// Auf Server einloggen
+		Client client = RESTful.login(URLS.LOGIN.getURL(), username, password);
+		ArrayList<Recording> recordings = RESTful.recordingOutput(client,
+				URLS.LIST_RECORDINGS.getURL()+"?page=2");
 
-			// Auf Server einloggen
-			Client client = RESTful.login(URLS.LOGIN.getURL(), username,
-					password);
-			ArrayList<Recording> recordings = RESTful.recordingOutput(client,
-					URLS.LIST_RECORDINGS.getURL() + "?page=2");
+		// Chart.drawSingleRecording(170, true, username, password);
 
-			// Chart.drawSingleRecording(170, true, username, password);
-
-			// Alle Charts aus dem Bremsvorgang-Test
-			for (int i = 0; i < recordings.size(); i++) {
-				int id = recordings.get(i).getId();
-				if (id >= 217) {
-					System.out.println("...processing id: " + id);
-					Chart.drawSingleRecording(recordings.get(i), true,
-							username, password);
-				}
+		// Alle Charts aus dem Bremsvorgang-Test
+		for (int i = 0; i < recordings.size(); i++) {
+			int id = recordings.get(i).getId();
+			if (id >= 217) {
+				System.out.println("...processing id: " + id);
+				Chart.drawSingleRecording(recordings.get(i), true, username, password);
 			}
-			// Chart.drawAllRecordings(recordings, username, password);
-			// Chart.drawSingleRecording(299, true, username, password);
+		}
+		// Chart.drawAllRecordings(recordings, username, password);
+		//Chart.drawSingleRecording(299, true, username, password);
 		}
 		System.out.println("done");
 
