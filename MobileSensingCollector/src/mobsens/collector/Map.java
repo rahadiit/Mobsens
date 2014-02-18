0b4a7a32f9efaffa0f3fc346852e5b8888e27a7c
Lukas Härtel <lukashaertel@googlemail.com>
2014-02-18 19:22:24 +0100

    Resource Overlay hinzugefügt

--- MobileSensingCollector/src/mobsens/collector/Map.java
+++ MobileSensingCollector/src/mobsens/collector/Map.java
@@ -1,28 +1,21 @@
 package mobsens.collector;
 
 import java.sql.Date;
-
-import java.util.ArrayList;
 import mobsens.collector.communications.ConnectingActivity;
 import mobsens.collector.drivers.messaging.CollectorStatusDriver;
 import mobsens.collector.drivers.messaging.LocationUpdateDriver;
 import mobsens.collector.intents.IntentStopCollector;
+import mobsens.collector.maps.ResourceOverlay;
 import mobsens.collector.util.Logging;
 
 import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.Overlay;
-import org.osmdroid.views.overlay.OverlayItem;
-import org.osmdroid.views.overlay.PathOverlay;
-
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -38,22 +31,12 @@ public class Map extends ConnectingActivity
 	private CollectorIPC collectorIPC;
 
 	private MapView mapViewMapPosition;
-	
-	private MapController mapController;
-	
-	private Overlay myLocationOverlay;
-	
-    private ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
-    
-    //private PathOverlay myPath = new PathOverlay(Color.RED, this);
 
 	private TextView textViewMapSpeed;
 
 	private Button buttonMapStop;
-	
-    private ResourceProxy mResourceProxy;
-    
-    private GeoPoint yourLocation = new GeoPoint(53554070, -2959520); 
+
+	private ResourceProxy mResourceProxy;
 
 	public Map()
 	{
@@ -64,28 +47,13 @@ public class Map extends ConnectingActivity
 			@Override
 			protected void onLocationUpdate(Date time, double latitude, double longitude, Float accuracy, Double altitude, Float bearing, Float speed)
 			{
+				Log.d("Map", "Updated location: " + latitude + ", " + longitude);
+
 				if (mapViewMapPosition != null)
 				{
 					mapViewMapPosition.getController().animateTo(new GeoPoint(latitude, longitude));
-					
-					//das hier sollte Theoretisch deine momentane position anzeigen
-					yourLocation.setLatitudeE6((int) latitude);
-					yourLocation.setLongitudeE6((int) longitude);
-					//yourLocation = new GeoPoint(latitude, longitude);
-					
-					//path
-					//myPath.addPoint(new GeoPoint(latitude, longitude));
-					
-					
-					//update current location marker
-					//overlays.clear();
-			        //overlays.add(new OverlayItem("New Overlay", "Your Location", yourLocation));
-			        //this.mapViewMapPosition.getOverlays().add(this.myLocationOverlay);
-			       
-					//update the map?
-					mapViewMapPosition.invalidate();
+					locationOverlay.setGeoPoint(new GeoPoint(latitude, longitude));
 
-			        
 					if (bearing != null)
 					{
 						mapViewMapPosition.setMapOrientation(bearing);
@@ -127,67 +95,32 @@ public class Map extends ConnectingActivity
 		collectorIPC = null;
 	}
 
+	private ResourceOverlay locationOverlay;
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		//old
+		// old
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		
-		//get a handle on the XML element
+
+		// get a handle on the XML element
 		mapViewMapPosition = (MapView) findViewById(R.id.map_position);
-		//Map parameters
-		textViewMapSpeed = (TextView) findViewById(R.id.map_speed);
-		buttonMapStop = (Button) findViewById(R.id.map_stop);
+
+		// Map parameters
 		mapViewMapPosition.setClickable(true);
 		mapViewMapPosition.setBuiltInZoomControls(true);
 		mapViewMapPosition.getController().setZoom(16);
 
-		
-		//new
-        mapController = (MapController) this.mapViewMapPosition.getController();
-        GeoPoint mapCenter = new GeoPoint(53554070, -2959520); 
-        GeoPoint overlayPoint = new GeoPoint(53554070 + 1000, -2959520 + 1000); 
-        //GeoPoint yourLocation = new GeoPoint(53554070, -2959520); 
-        mapController.setCenter(mapCenter);
-		
-        //create an ArrayList of 'OverlayItems'. To this list, we will add the overlays we wish to add to the OSM. 
-        //ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
-        overlays.add(new OverlayItem("New Overlay", "Overlay Description", overlayPoint));
-        overlays.add(new OverlayItem("New Overlay", "Your Location", yourLocation));
-        //myPath.addPoint(mapCenter);
-        
-        //PathOverlay myPath = new PathOverlay(Color.RED, this);
-        
-        
-		
-        // instantiate the ItemizedIconOverlay class
-        mResourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
-        this.myLocationOverlay = new ItemizedIconOverlay<OverlayItem>(overlays, null, mResourceProxy);
-        //mapViewMapPosition.getOverlays().add(myPath);
-        this.mapViewMapPosition.getOverlays().add(this.myLocationOverlay);
-        
-        mapViewMapPosition.invalidate();
-        
-        /*
-		//new
-		overlayItemArray = new ArrayList<OverlayItem>();
-		DefaultResourceProxyImpl defaultResourceProxyImpl 
-        = new DefaultResourceProxyImpl(this);
-		MyItemizedIconOverlay myItemizedIconOverlay = new MyItemizedIconOverlay(
-          overlayItemArray, null, defaultResourceProxyImpl);
-		mapViewMapPosition.getOverlays().add(myItemizedIconOverlay);
-		
-		
-		//new
-		//Default Ortsmarker
-		OverlayItem newMyLocationItem = new OverlayItem(
-			       "My Location", "My Location", new GeoPoint(50.363566, 7.559343));
-	    overlayItemArray.add(newMyLocationItem);
-		*/
-	    
+		mResourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+
+		textViewMapSpeed = (TextView) findViewById(R.id.map_speed);
+		buttonMapStop = (Button) findViewById(R.id.map_stop);
+
+		locationOverlay = new ResourceOverlay(this);
+		locationOverlay.setResource(R.drawable.ic_launcher);
+
+		mapViewMapPosition.getOverlayManager().add(locationOverlay);
 
 		buttonMapStop.setOnClickListener(new OnClickListener()
 		{
@@ -284,11 +217,4 @@ public class Map extends ConnectingActivity
 		mapViewMapPosition.setMapOrientation(savedInstanceState.getFloat(R.id.map_position + "orientation"));
 	}
 
-
-	
-
-	
-	}
-
-
-
+}
