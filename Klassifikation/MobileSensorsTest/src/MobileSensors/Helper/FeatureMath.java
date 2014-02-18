b13ed9cc9beccd20f1e819d55e00af1bca39b130
hennyb <henny@uni-koblenz.de>
2014-02-18 17:13:19 +0100

    main aufgeraeumt

--- Klassifikation/MobileSensorsTest/src/MobileSensors/Helper/FeatureMath.java
+++ Klassifikation/MobileSensorsTest/src/MobileSensors/Helper/FeatureMath.java
@@ -1,100 +1,95 @@
 package MobileSensors.Helper;
 
-import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 public class FeatureMath {
 
-	public static double sum (double[] xs) {
-		
+	public static double sum(double[] xs) {
 		DescriptiveStatistics ds = new DescriptiveStatistics();
-		
+
 		for (double x : xs) {
-			
 			ds.addValue(x);
-			
 		}
 		
 		return ds.getSum();
-		
+
 	}
-	
-	public static double arithMean (double[] xs) {
-		
+
+	public static double arithMean(double[] xs) {
+
 		DescriptiveStatistics ds = new DescriptiveStatistics();
-		
+
 		for (double x : xs) {
-			
+
 			ds.addValue(x);
-			
+
 		}
-		
+
 		return ds.getMean();
-		
+
 	}
-	
-	public static double geomMean (double[] xs) {
-		
+
+	public static double geomMean(double[] xs) {
+
 		DescriptiveStatistics ds = new DescriptiveStatistics();
-		
+
 		for (double x : xs) {
-			
+
 			ds.addValue(x);
-			
+
 		}
-				
+
 		return ds.getGeometricMean();
 	}
-	
-	public static double harmMean (double[] xs) {
-		
+
+	public static double harmMean(double[] xs) {
+
 		double harm = 0;
-		
+
 		for (double x : xs) {
-			
-			harm += 1/x;
-			
+
+			harm += 1 / x;
+
 		}
-		
+
 		harm /= xs.length;
-		
+
 		return harm;
-		
+
 	}
-	
-	public static double variance (double[] xs) {
-		
+
+	public static double variance(double[] xs) {
+
 		DescriptiveStatistics ds = new DescriptiveStatistics();
-		
+
 		for (double x : xs) {
-			
+
 			ds.addValue(x);
-			
+
 		}
-				
+
 		return ds.getVariance();
-		
+
 	}
-	
-	public static double s2 (double[] xs) {
-		
+
+	public static double s2(double[] xs) {
+
 		return Math.sqrt(FeatureMath.variance(xs));
-		
+
 	}
-	
-	public static double kurtosis (double[] xs) {
-		
+
+	public static double kurtosis(double[] xs) {
+
 		DescriptiveStatistics ds = new DescriptiveStatistics();
-		
+
 		for (double x : xs) {
-			
+
 			ds.addValue(x);
-			
+
 		}
-				
+
 		return ds.getKurtosis();
-		
+
 	}
-	
-	
+
 }
--- Klassifikation/MobileSensorsTest/src/MobileSensors/Test/Main.java
+++ Klassifikation/MobileSensorsTest/src/MobileSensors/Test/Main.java
@@ -1,31 +1,25 @@
 package MobileSensors.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
 
-import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.io.FileUtils;
 
 import com.sun.jersey.api.client.Client;
 
-import MobileSensors.Enums.AcceleroOption;
-import MobileSensors.Enums.Axis;
-import MobileSensors.Helper.AccelerationFeatureVector;
 import MobileSensors.Storage.Sensors.Accelerometer;
-import MobileSensors.Storage.Sensors.Location;
 import MobileSensors.Test.Data.Recording;
 import MobileSensors.Test.Data.SensorE;
 import MobileSensors.Test.Data.URLS;
 import MobileSensors.Test.Input.CSV;
 import MobileSensors.Test.Input.RESTful;
-import MobileSensors.Test.Output.Chart;
 import MobileSensors.Test.Output.WekaFile;
 
 public class Main {
 
 	private static final String username = "mlukas@gmx.net";
-	private static final String password = "12345678";
+	private static final String password = "12345678";	
 
 	public static void main(String[] args) throws IOException {
 
@@ -34,41 +28,31 @@ public class Main {
 		ArrayList<Recording> recordings = RESTful.recordingOutput(client,
 				URLS.LIST_RECORDINGS.getURL()); // + "?page=" + j
 
-		// Chart.drawSingleRecording(170, true, username, password);
+		
+		// String acceleroCSV = FileUtils.readFileToString(new
+		// File("accelero.csv"));
+		// ArrayList<Accelerometer> accelerometer = CSV.csvToSensor(acceleroCSV,
+		// Accelerometer.class);
 
-		// Alle Charts aus dem Bremsvorgang-Test
-		for (int i = 0; i < recordings.size(); i++) {
-			int id = recordings.get(i).getId();
-			 if (id == 170) {
-			//if (recordings.get(i).getTitle().toLowerCase().contains("auswe")) {
-				//System.out.println("...processing id: " + id);
-				// hier Optionen und Achsen waehlen.
-
-				 
-//				Chart.drawSingleRecording(recordings.get(i), true, username,
-//						password);
-				 
-				 String acceleroCSV = RESTful.getCSV(client, recordings.get(i).getId(), URLS.CSV.getURL(),
+		boolean schleifeausfuehren = false;
+		if (schleifeausfuehren) {
+			// Alle Charts aus dem Bremsvorgang-Test
+			for (int i = 0; i < recordings.size(); i++) {
+				int id = recordings.get(i).getId();
+				if (id == 170) {
+					
+					String acceleroCSV = RESTful.getCSV(client,
+							recordings.get(i).getId(), URLS.CSV.getURL(),
 							SensorE.ACCELEROMETERS);
-				 ArrayList<Accelerometer> accelerometer = CSV.csvToSensor(acceleroCSV,
-								Accelerometer.class);
-				 
-				 WekaFile.writeFile(accelerometer, 1000, "Weka.csv");
-				 
-				 
-							
+
+					ArrayList<Accelerometer> accelerometer = CSV.csvToSensor(
+							acceleroCSV, Accelerometer.class);
+
+					WekaFile.writeFile(accelerometer, 1000, "Weka.csv");
+
+				}
 			}
-			// }
 		}
-
-		// Chart.drawAllRecordings(recordings, username, password);
-		// Chart.drawSingleRecording(299, true, username, password);
-
 		System.out.println("done");
-
 	}
 }
-
-// ALTER CODE
-// + DateFormatUtils.format(new Date(locations.get(0).getTime()),
-// "MM-dd HH:mm:ss"));
