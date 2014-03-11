0e14e8a9165cbdb4e21aae612f56ae2ae7027208
Lukas Härtel <lukashaertel@googlemail.com>
2014-03-11 12:09:03 +0100

    Konstanten Eingeführt

---
+++ MobileSensingCollector/src/mobsens/collector/config/Constants.java
@@ -0,0 +1,23 @@
+package mobsens.collector.config;
+
+public interface Constants
+{
+	public static final String ADDRESS_UPLOAD = "http://mobilesensing.west.uni-koblenz.de/recordings/upload";
+	
+	public static final String ADDRESS_LOGIN = "http://mobilesensing.west.uni-koblenz.de/users/sign_in.json";
+	
+	public static final String PREFERENCES_NAME = "mscprefs";
+	
+	public static final String PREFERENCE_EMAIL = "mscemail";
+	
+	public static final String PREFERENCE_PASSWORD = "mscpwd";
+	
+	public static final String PREFERENCE_STARTUP = "mscsup";
+
+	public static final float METERS_PER_KILOMETER = 1000.0f;
+
+	public static final float SECONDS_PER_HOUR = 3600.0f;
+
+	public static final float PATH_STROKE_WIDTH = 10.0f;
+
+}
