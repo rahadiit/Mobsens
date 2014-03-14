fa7f8c555c838091fec4ca1fe76113bb78d37296
deviljs <maxmeffert@uni-koblenz.de>
2014-03-14 11:50:36 +0100

    some changes, currently broken

---
+++ Klassifikation/MobileSensors/src/MobileSensors/Events/EventTrainer.java
@@ -0,0 +1,16 @@
+package MobileSensors.Events;
+
+import MobileSensors.Events.ARFactories.ARFactory;
+
+public class EventTrainer {
+
+	private int windowWidth;
+	private int windowDelta;
+	private ARFactory eventARFactory;
+	private Instances eventTrainingSet;
+	private Classifier eventClassifier;
+	private Evaluation eventClassifiervaluation;
+	
+	
+	
+}
