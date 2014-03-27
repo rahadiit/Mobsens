5f7e6521c2872493c2e270510c972761da988562
deviljs <maxmeffert@uni-koblenz.de>
2014-03-27 20:48:51 +0100

    remove unused imports

--- Klassifikation/MobileSensors/src/MobileSensors/Events/Trainers/BrakeTrainer.java
+++ Klassifikation/MobileSensors/src/MobileSensors/Events/Trainers/BrakeTrainer.java
@@ -1,23 +1,9 @@
 package MobileSensors.Events.Trainers;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Random;
-
-import weka.classifiers.Classifier;
-import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
-import weka.core.Instances;
-import weka.core.SerializationHelper;
-import MobileSensors.MobSens;
 import MobileSensors.Events.ARFactories.BrakeARFactory;
-import MobileSensors.Events.ARFactories.DodgeARFactory;
-import MobileSensors.Events.Labels.*;
-import MobileSensors.Sensors.Accelerometer;
-import MobileSensors.Sensors.SensorRecord;
+import MobileSensors.Events.Labels.BrakeLabel;
 
 public class BrakeTrainer extends EventTrainer<BrakeLabel> {
 
--- Klassifikation/MobileSensors/src/MobileSensors/Events/Trainers/KerbstoneTrainer.java
+++ Klassifikation/MobileSensors/src/MobileSensors/Events/Trainers/KerbstoneTrainer.java
@@ -1,24 +1,9 @@
 package MobileSensors.Events.Trainers;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Map;
-import java.util.Random;
-
-import weka.classifiers.Classifier;
-import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
-import weka.core.Instances;
-import weka.core.SerializationHelper;
-import MobileSensors.MobSens;
-import MobileSensors.Events.ARFactories.BrakeARFactory;
 import MobileSensors.Events.ARFactories.KerbstoneARFactory;
 import MobileSensors.Events.Labels.KerbstoneLabel;
-import MobileSensors.Sensors.Accelerometer;
-import MobileSensors.Sensors.SensorRecord;
 
 /**
  * 
