80ed633208121e72c4c953caf3a4cdd8fc8b9325
deviljs <maxmeffert@uni-koblenz.de>
2014-03-27 20:48:51 +0100

    remove unused imports

--- Klassifikation/MobileSensors/src/MobileSensors/Events/Trainers/DodgeTrainer.java
+++ Klassifikation/MobileSensors/src/MobileSensors/Events/Trainers/DodgeTrainer.java
@@ -1,24 +1,9 @@
 package MobileSensors.Events.Trainers;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Random;
-
-import weka.classifiers.Classifier;
-import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
-import weka.core.Instances;
-import weka.core.SerializationHelper;
-import MobileSensors.MobSens;
-import MobileSensors.Events.ARFactories.ARFactory;
-import MobileSensors.Events.ARFactories.BrakeARFactory;
 import MobileSensors.Events.ARFactories.DodgeARFactory;
 import MobileSensors.Events.Labels.DodgeLabel;
-import MobileSensors.Sensors.Accelerometer;
-import MobileSensors.Sensors.SensorRecord;
 
 /**
  * 
