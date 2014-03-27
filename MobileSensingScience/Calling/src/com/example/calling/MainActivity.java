170614f3ac773d4deb5e307b2e90db751a67aa99
hennyb <henny@uni-koblenz.de>
2014-03-27 15:15:19 +0100

    fixed deprecated code und fp-code aktualisierung

--- MobileSensingScience/Calling/src/com/example/calling/MainActivity.java
+++ MobileSensingScience/Calling/src/com/example/calling/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calling;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
@@ -21,11 +22,13 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void process(View view) {
+	public void process(View view) throws InterruptedException {
 		for (int i = 1; i < 40; i++) {
+
 			startNewActivity("com.example.ziptest", i + "000");
 			try {
-				Thread.sleep(20000);
+				int delay = Integer.parseInt(i + "000") + 5000;
+				Thread.sleep(delay);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -36,7 +39,21 @@ public class MainActivity extends Activity {
 	public void startNewActivity(String packageName, String data) {
 		Intent launchIntent = getPackageManager().getLaunchIntentForPackage(
 				packageName);
+
 		launchIntent.putExtra("data", data);
-		startActivity(launchIntent);
+		startActivityForResult(launchIntent, 0);
 	}
+
 }
+
+/*
+ * dead code
+ * 
+ * @Override protected void onActivityResult(int requestCode, int resultCode,
+ * Intent data) { // super.onActivityResult(requestCode, resultCode, data); if
+ * (resultCode == RESULT_OK) {
+ * 
+ * try { Log.i("MA", "Result" + data.getIntExtra("value", 0));
+ * doIt(data.getIntExtra("value", 0)); } catch (Exception e) { Log.w("MA", "" +
+ * e.toString()); } } else { Log.i("MA", "errror" + resultCode); } }
+ */
